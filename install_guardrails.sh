840b5435ae462069c09f91fb85fecacf4e87679e
m-misiura <mmisiura@redhat.com>
2024-08-28 11:47:26 +0100

    :construction: added two shell scripts to: 1) authenticate guardrails package; 2) install desired guardrails in a virtual env

---
+++ install_guardrails.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install packages from Guardrails Hub
+guardrails hub install hub://guardrails/regex_match
+guardrails hub install hub://guardrails/competitor_check
+guardrails hub install hub://guardrails/toxic_language
