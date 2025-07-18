34a407d07a5734d6e91d0b337e3284c80e8b4799
amberjblue <ablue@redhat.com>
2025-07-18 09:18:42 -0400

    cleanup conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,21 +7,17 @@ import pytest
 def setup_imports():
     """Setup Python path for imports"""
     project_root = os.path.dirname(os.path.dirname(__file__))
-    src_path = os.path.join(project_root, "src")
     detectors_path = os.path.join(src_path, "guardrails_detectors")
     huggingface_path = os.path.join(detectors_path, "huggingface")
     llm_judge_path = os.path.join(detectors_path, "llm_judge")
     built_in_detectors_path = os.path.join(detectors_path, "built_in")
-    common_path = os.path.join(detectors_path, "common")
     
     paths = [
-        src_path,
-        detectors_path,
         huggingface_path,
+        detectors_path,
+        project_root,
         llm_judge_path,
         built_in_detectors_path,
-        common_path,
-        project_root,
     ]
 
     for path in paths:
